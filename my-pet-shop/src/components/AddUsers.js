ea5d416db99c5ca08428c8ef1ab4cbcdd83d4c7a
JhaelArce <jhael.arce.8041022@usalesiana.edu.bo>
2023-11-11 12:16:59 -0400

    createHeaderAndFooterUpdate

--- my-pet-shop/src/components/AddUsers.js
+++ my-pet-shop/src/components/AddUsers.js
@@ -1,28 +1,32 @@
 import React, { useState } from "react";
-import uniqid from 'uniqid';
 import axios from 'axios';
 import '../Css/AddUser.css';
+import uniqid from 'uniqid';
+import Header from './Header.js'; 
+import Footer from "./Footer.js";
 
 function AddUser() {
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
+    const [userName, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [username, setUsername] = useState('');
+    const [customValidationMessage, setCustomValidationMessage] = useState('');
 
-    function signUpUser() {
+    function signUpUser(e) {
+        e.preventDefault();
+
         const user = {
             firstName: firstName,
             lastName: lastName,
+            userName: userName,
             email: email,
             password: password,
-            username: username,
             userId: uniqid()
         };
 
         console.log(user);
 
-        // Aquí puedes enviar los datos del usuario al servidor o realizar las acciones necesarias.
         axios.post('/api/user/adduser', user)
             .then(res => {
                 alert(res.data);
@@ -32,65 +36,90 @@ function AddUser() {
             });
     }
 
+    function handleInputChange(e) {
+        setCustomValidationMessage('');
+    }
+
     return (
-        <div className={`container white-container`}>
-
-            <div className="row ">
-                <h2 className="mt-4">Sign Up</h2>
-            </div>
-
-            <div className="row">
-                <div className="col-sm-6 offset-3">
-                    <div className="gray-background p-3">
-                        <div className="form-group">
-                            <label htmlFor="firstName">First Name</label>
-                            <input type="text" className="form-control" value={firstName} onChange={(e) => { setFirstName(e.target.value) }} />
-                        </div>
-
-                        <div className="form-group">
-                            <label htmlFor="lastName">Last Name</label>
-                            <input type="text" className="form-control" value={lastName} onChange={(e) => { setLastName(e.target.value) }} />
-                        </div>
-
-                        <div className="form-group">
-                            <label htmlFor="username">Username</label>
-                            <input type="text" className="form-control" value={username} onChange={(e) => { setUsername(e.target.value) }} />
-                        </div>
-
-                        <div className="form-group">
-                            <label htmlFor="email">Email</label>
-                            <input type="email" className="form-control" value={email} onChange={(e) => { setEmail(e.target.value) }} />
-                        </div>
-
-                        <div className="form-group">
-                            <label htmlFor="password">Password</label>
-                            <input type="password" className="form-control" value={password} onChange={(e) => { setPassword(e.target.value) }} />
-                        </div>
-
-                        <div className="button-container">
-                            <button onClick={signUpUser} className="btn btn-success">Sign Up</button>
-                        </div>
-
-                        <div className="button-container">
-                            <button className="btn-google">
-                                <img src="https://static.vecteezy.com/system/resources/previews/010/353/285/original/colourful-google-logo-on-white-background-free-vector.jpg" alt="Google Logo" />
-                              Google
-                            </button>
-                        </div>
-
-                        <div className="button-container">
-    <button className="btn-facebook-alternate">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1200px-Facebook_f_logo_%282019%29.svg.png" alt="Facebook Logo" />
-        Facebook
-    </button>
-</div>
+        <div>
+            <Header />
+            <form className="form" onSubmit={signUpUser} noValidate>
+                <p className="title">Register</p>
+                <p className="message">Register now and get full access to other features</p>
+                <div className="form-fields">
+                    <div className="form-field">
+                        <label htmlFor="firstName">First Name</label>
+                        <input
+                            required
+                            type="text"
+                            id="firstName"
+                            className="input"
+                            value={firstName}
+                            onChange={(e) => { setFirstName(e.target.value); handleInputChange(e); }}
+                        />
+                    </div>
+                    <div className="form-field">
+                        <label htmlFor="lastName">Last Name</label>
+                        <input
+                            required
+                            type="text"
+                            id="lastName"
+                            className="input"
+                            value={lastName}
+                            onChange={(e) => { setLastName(e.target.value); handleInputChange(e); }}
+                        />
+                    </div>
+                    <div className="form-field">
+                        <label htmlFor="username">Username</label>
+                        <input
+                            required
+                            type="text"
+                            id="username"
+                            className="input"
+                            value={userName}
+                            onChange={(e) => { setUsername(e.target.value); handleInputChange(e); }}
+                        />
+                    </div>
+                    <div className="form-field">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            required
+                            type="email"
+                            id="email"
+                            className="input"
+                            value={email}
+                            onChange={(e) => { setEmail(e.target.value); handleInputChange(e); }}
+                        />
+                    </div>
+                    <div className="form-field">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            required
+                            type="password"
+                            id="password"
+                            className="input"
+                            value={password}
+                            onChange={(e) => { setPassword(e.target.value); handleInputChange(e); }}
+                        />
                     </div>
                 </div>
+                <button className="submit" type="submit">Sign Up</button>
+                <p className="signin">Register with</p>
+                <div className="social-buttons-container">
+                <button className="social-button google-button">
+                    <img src="https://static.vecteezy.com/system/resources/previews/010/353/285/original/colourful-google-logo-on-white-background-free-vector.jpg" alt="Google" />
+                    Google
+                </button>
+                <button className="social-button facebook-button">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1200px-Facebook_f_logo_%282019%29.svg.png" alt="Facebook" />
+                    Facebook
+                </button>
             </div>
-            
+                <p className="signin">Already have an account? <a href="#">Sign In</a></p>
+            </form>
+            {customValidationMessage && <p style={{ color: 'red' }}>{customValidationMessage}</p>}
+            <Footer />
         </div>
-
-        
     );
 }
 
---
+++ my-pet-shop/src/components/Footer.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import '../Css/Footer.css'; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTiktok, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="column">
+        <img src="https://www.mypetshop.co.za/wp-content/uploads/2019/11/My-petshop-LOGO.png" alt="Logo" className="logo" />
+        <p>Follow us</p>
+        <div className="social-icons">
+          <FontAwesomeIcon icon={faFacebook} className="social-icon" />
+          <FontAwesomeIcon icon={faTiktok} className="social-icon" />
+          <FontAwesomeIcon icon={faInstagram} className="social-icon" />
+        </div>
+      </div>
+      <div className="column">
+        <ul className="menu-list">
+        <li>Home</li>
+           <li>About</li>
+           <li>Services</li>
+           <li>Contact</li>
+        </ul>
+      </div>
+      <div className="column">
+        <ul className="menu-list">
+        <li>Terms and conditions</li>
+           <li>Privacy policy</li>
+           <li>Frequently asked questions</li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
+
